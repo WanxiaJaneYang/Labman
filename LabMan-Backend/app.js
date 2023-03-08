de24f1095807ac6a50047d41b12eaf71b6fbca87
kaini <a1866621@adelaide.edu.au>
2023-03-08 22:07:51 +1030

    config changed

--- LabMan-Backend/app.js
+++ LabMan-Backend/app.js
@@ -3,31 +3,48 @@ import mysql from "mysql";
 import fs from "fs";
 const app = express();
 
-var config =
-{
+const pool = mysql.createPool({
+
   host:"labman.mysql.database.azure.com", 
   user:"a1866621", 
   password:"Adelaide123N", 
   database:"labman", 
   port:3306, 
-  ssl: {    rejectUnauthorized: false,
-    ca: fs.readFileSync("./BaltimoreCyberTrustRoot.crt.pem", "utf8")}
-};
-const conn = new mysql.createConnection(config);
-
-conn.connect(
-    function (err) { 
+  connectTimeout: 60000,
+  ssl: {   
+    rejectUnauthorized: false,
+    // ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")
+}
+});
+pool.getConnection(function(err, connection) {
     if (err) { 
         console.log("!!! Cannot connect !!! Error:");
         throw err;
-    }
-    else
+    }    else
     {
        console.log("Connection established.");
-        //    queryDatabase();
+            readData(connection);
     }
+
+  // perform database operations using the connection
+
+//   connection.release(); // release the connection back to the pool
 });
 
+
+function readData(connection){
+    connection.query('SELECT * FROM test', 
+        function (err, results, fields) {
+            if (err) throw err;
+            else console.log(results);
+            console.log('Done.');
+        })
+        connection.release(
+        function (err) { 
+            if (err) throw err;
+            else  console.log('Closing connection.') 
+    });
+};
 app.use(express.json());
 
 export default app;
