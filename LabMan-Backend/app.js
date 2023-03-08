4776281098235d9e065f6f2a473db5ddb2770323
kaini <a1866621@adelaide.edu.au>
2023-03-08 19:55:07 +1030

    connect to cloud database server & create folder for project management

--- LabMan-Backend/app.js
+++ LabMan-Backend/app.js
@@ -1,6 +1,33 @@
 import express from "express";
-
+import mysql from "mysql";
+import fs from "fs";
 const app = express();
+
+var config =
+{
+  host:"labman.mysql.database.azure.com", 
+  user:"a1866621", 
+  password:"Adelaide123N", 
+  database:"mysql", 
+  port:3306, 
+  ssl: {    rejectUnauthorized: false,
+    ca: fs.readFileSync("./BaltimoreCyberTrustRoot.crt.pem", "utf8")}
+};
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+    function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+       console.log("Connection established.");
+        //    queryDatabase();
+    }
+});
+
 app.use(express.json());
 
 export default app;
