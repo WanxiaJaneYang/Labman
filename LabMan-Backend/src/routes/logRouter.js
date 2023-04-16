e1cf3d98de9cb8fe76841cc9cc935fa5f9355244
kaini <a1866621@adelaide.edu.au>
2023-04-16 14:51:39 +0930

    handle failure of database connection

--- LabMan-Backend/src/routes/logRouter.js
+++ LabMan-Backend/src/routes/logRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAlllogs} from '../controllers/log/getAllLogs.js';
+import { getAllLogs} from '../controllers/logs/getAllLogs.js';
 
 
 
 const logRouter = Router();
 
 //query all users
-logRouter.get("/", getAlllogs);
+logRouter.get("/", getAllLogs);
   
 
 export { logRouter };
--- LabMan-Backend/src/utils/MySQL/db.js
+++ LabMan-Backend/src/utils/MySQL/db.js
@@ -5,24 +5,35 @@ import fs from "fs";
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DBNAME,
-    port: process.env.DBPORT,
-    connectTimeout: 60000,
-    ssl: {
-      rejectUnauthorized: false,
-      ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")
-    },
-    acquireTimeout: 10000,
-    waitForConnections: true
-  });
-  
+  host: process.env.HOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME,
+  port: process.env.DBPORT,
+  connectTimeout: 60000,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")
+  },
+  acquireTimeout: 10000,
+  waitForConnections: true
+});
+console.log("Connection pool object created successfully");
+
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('database is not connectable: ', err);
+  } else {
+    console.log('database is connectable ');
+    connection.release();
+  }
+});
+
 function connectToDatabase() {
-    return pool;
+  return pool;
 }
-  
-export {connectToDatabase};
+
+export { connectToDatabase };
 export default pool;
 
