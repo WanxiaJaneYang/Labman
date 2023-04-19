18ffa4c5536340d96c69de57b93d4ade7b64393c
kaini <a1866621@adelaide.edu.au>
2023-04-19 14:20:56 +0930

    collect request

--- LabMan-Backend/src/routes/requestRouter.js
+++ LabMan-Backend/src/routes/requestRouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { getRequests } from '../controllers/request/getRequests.js';
 import { newRequest } from '../controllers/request/newRequest.js';
-// import { pickRequest } from '../controllers/request/pickRequest.js';
+import { collectRequest } from '../controllers/request/collectRequest.js';
 
 const requestRouter = Router();
 
@@ -11,8 +11,8 @@ requestRouter.get("/", getRequests);
 // Create a new reqest record and insert a log into request Log table
 requestRouter.post("/", newRequest);
 
-// // Create a new borrowing record and insert a log into Equipment Log table
-// requestRouter.post("/pick", pickRequest);
+// Create a new borrowing record and insert a log into Equipment Log table
+requestRouter.post("/collect", collectRequest);
 
 
 export { requestRouter };
