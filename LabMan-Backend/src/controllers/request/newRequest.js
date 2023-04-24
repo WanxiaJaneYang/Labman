49bfd932e2ba53747d9d7f9f9c17fe6e19d82d53
kaini <a1866621@adelaide.edu.au>
2023-04-24 21:43:48 +0930

    request backend code improvement

--- LabMan-Backend/src/controllers/request/newRequest.js
+++ LabMan-Backend/src/controllers/request/newRequest.js
@@ -1,45 +1,46 @@
 import moment from "moment";
+import runTransaction from "./transaction.js";
 import pool from "../../utils/MySQL/db.js";
-async function newRequest(req, res) {
-    let connection;
+
+function newRequest(req, res) {
     try {
         const { type_id, type_name, student_id, borrow_amount, return_date } = req.body;
+        // Get the current date and time
+        const current_time = moment().format("YYYY-MM-DD HH:mm:ss");
 
-			// Get the current date and time
-			const current_time = moment().format("YYYY-MM-DD HH:mm:ss");
+        // Create new request record
+        const requestRecord = {
+            student_id,
+            type_id,
+            type_name,
+            borrow_amount,
+            request_time: current_time,
+            return_date,
+            request_status: 0, // 0 = pending/new request
+        };
 
-            // Create new request record
-            const requestRecord = {
-                student_id,
-                type_id,
-                type_name,
-                borrow_amount,
-                request_time: current_time,
-                return_date,
-                request_status: 0, // 0 = pending/new request
-            };
+        //check if the available amount of type_id more than 0
+        const sql = 'SELECT available_amount FROM equipment_type WHERE type_id = ?';
 
-            // Get a connection from the pool
-            pool.getConnection((error, conn) => {
-                if (error) {
-                    console.error(error);
-                    res.status(500).json({ error: 'Failed to connect to database' });
-                }
-                connection = conn;
+        // Execute the SQL query with the request_id parameter
+        pool.query(sql, [type_id], (error, results) => {
+            if (error) {
+                console.error(err);
+                return;
+            }
+            //console.log(results);
 
-                // Start a transaction
-                connection.beginTransaction((error) => {
-                    if (error) {
-                        console.error(error);
-                    }
-
-                    // Insert requestRecord into requests table
+            const amount = results[0].available_amount;
+            //console.log(amount);
+            if (amount > borrow_amount) {
+                runTransaction(async (connection) => {
+                    //Insert requestRecord into requests table
                     connection.query('INSERT INTO requests SET ?', requestRecord, (error, resultId) => {
                         if (error) {
                             console.error(error);
-                            res.status(500).json({ error: 'Failed to find table requests' });
-
+                            return res.status(500).json({ error: 'Failed to insert record into table requests' });
                         }
+                        //console.log(resultId);
                         const insertId = resultId.insertId;
 
                         // Create a new request log for the new request
@@ -57,40 +58,22 @@ async function newRequest(req, res) {
                         connection.query('INSERT INTO request_Log SET ?', requestLog, (error) => {
                             if (error) {
                                 console.error(error);
-                                res.status(500).json({ error: 'Failed to find table request_Log' });
+                                return res.status(500).json({ error: 'Failed to find table request_Log' });
                             }
-
-                            // Commit the transaction
-                            connection.commit((error) => {
-                                if (error) {
-                                    console.error(error);
-                                    res.status(500).json({ error: 'Failed to commit the transaction' });
-                                }
-
-                                // Release the connection back to the pool
-                                connection.release();
-
-                                // Send response indicating success
-                                res.status(200).json({ success: 'Request record and log created successfully' });
-                            });
+                            return res.status(200).json({ success: "Request record and log created successfully" });
                         });
                     });
                 });
-            });
+            } else {
+                res.status(500).json({ error: "not available" });
+            }
+        })
+    } catch (error) {
+        console.error(error);
 
-    } catch (err) {
-        console.error(err);
-
-		// Rollback the transaction on error
-		if (connection) {
-			connection.rollback(() => {
-				connection.release();
-			});
-		}
-
-		// Send error response
-		res.status(500).json({ error: "Failed to create new request" });
-	}
+        // Send error response
+        res.status(500).json({ error: "Failed to create new request" });
+    }
 }
 
 export { newRequest };
