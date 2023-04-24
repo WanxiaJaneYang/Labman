6dd25cdcc0cac828dee14dbf7f8974e6b4bc4371
WanxiaJaneYang <1016887513@qq.com>
2023-04-24 16:39:38 +0930

    implement front end api calling functions

--- LabMan-Frontend/src/pages/admin/ReturnPage/Components/Modals/ShowDetailModal/index.js
+++ LabMan-Frontend/src/pages/admin/ReturnPage/Components/Modals/ShowDetailModal/index.js
@@ -1,13 +1,36 @@
 import { Modal } from "antd";
+import { useReturnRecordContext } from "../../../ReturnRecordContext";
 
-const ShowDetailModal = (props) => {
-	const okHandler = () => {
-		// call the API to get the details of the selected request record here
-		props.hideModal();
+const ShowDetailModal = () => {
+	const { modalVisible, setModalVisible , modalData} = useReturnRecordContext();
+
+	const hideModal = () => {
+		setModalVisible(false);
+	};
+
+	const formatDate = (date) => {
+		return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 	};
 
 	return (
-		<Modal title='Request Details' width="70vw" open={props.open} onCancel={props.hideModal} onOk={okHandler} selectedRow={props.selectedRow}></Modal>
+		<Modal 
+			title='Borrow Record Details' 
+			width="70vw" 
+			open= {modalVisible}
+			onCancel={hideModal} 
+			onOk={hideModal} 
+			maskStyle={{backgroundColor: "rgba(0,0,0,0.3)"}}
+		>
+			<p>Borrow Date: {modalData.borrow_date}</p>
+			<p>Due Date: {formatDate(modalData.return_date)}</p>
+			<p>Student ID: {modalData.student_id}</p>
+			<p>Equipment Name: {modalData.type_name}</p>
+			<p>Amount: {modalData.borrow_amount}</p>
+			<p>Status: {modalData.status==0? "To Return":"Returned"}</p>
+			<p style={
+				modalData.status==0?{display:"none"}:{display:"block"}
+			}>Actual Return Date:{formatDate(modalData.actual_return_date)}</p>
+		</Modal>
 	);
 };
 
--- LabMan-Frontend/src/pages/admin/ReturnPage/Components/Tables/ReturnTable/index.js
+++ LabMan-Frontend/src/pages/admin/ReturnPage/Components/Tables/ReturnTable/index.js
@@ -1,31 +1,53 @@
 import { Table } from "antd";
 import { useReturnRecordContext } from "../../../ReturnRecordContext";
+import ShowDetailModal from "../../Modals/ShowDetailModal";
+import { useEffect } from "react";
+
 const ReturnTable = () => {
-	const { data, loading, setSelectedRows, handleTableChange, tableParams} = useReturnRecordContext();
+	const { 
+		data, 
+		fetchData, 
+		loading, 
+		selectedRows, 
+		setSelectedRows, 
+		tableParams, 
+		setTableParams,
+		equipmentTypeList, 
+		getEquipmentTypeList,
+		setModalData,
+		setModalVisible,
+	} = useReturnRecordContext();
+
 	const columns = [
+		{
+			title: "Equipment Name",
+			dataIndex: "type_name",
+			filters: 
+				equipmentTypeList? equipmentTypeList.map((item) => {
+					return {
+						text: item.type_name,
+						value: item.type_name,
+					};
+				}):[],
+			render: (text, record) => {
+				return (
+					<>
+						<a onClick={()=>{
+							setModalData(record);
+							setModalVisible(true);
+						}}>{text}</a>
+						<ShowDetailModal/>
+					</>
+				);
+			}
+		},
 		{
 			title: "Borrow Time",
 			dataIndex: "borrow_date",
-			sorter: true,
 		},
 		{
 			title: "Due Time",
 			dataIndex: "return_date",
-			sorter: true,
-		},
-		{
-			title: "Equipment Name",
-			dataIndex: "type_name",
-			filters: [
-				{
-					text: "Macbook Pro",
-					value: "Macbook Pro",
-				},
-				{
-					text: "Macbook Air",
-					value: "Macbook Air",
-				},
-			],
 		},
 		{
 			title: "Student ID",
@@ -39,15 +61,36 @@ const ReturnTable = () => {
 	];
 
 	const rowSelection = {
+		selectedRowKeys: selectedRows ? selectedRows.map((row) => row.borrow_id) : [],
 		onChange: (selectedRowKeys, selectedRows) => {
 			console.log(`selectedRowKeys: ${selectedRowKeys}`, "selectedRows: ", selectedRows);
 			setSelectedRows(selectedRows);
 		},
-		getCheckboxProps: (record) => ({
-			disabled: record.name === "Disabled User",
-			// Column configuration not to be checked
-			name: record.name,
-		}),
+	};
+
+	useEffect(() => {
+		fetchData();
+		getEquipmentTypeList();
+	}, []);
+
+	useEffect(() => {
+		setTableParams({
+			...tableParams,
+			pagination: {
+				...tableParams.pagination,
+				total: data?data.length:0,}});
+	}, [data]);
+
+
+	const handleTableChange = (pagination, filters) => {
+		setTableParams({
+			pagination: {
+				...tableParams.pagination,
+				current: pagination.current,
+				pageSize: pagination.pageSize,
+			},
+			filters,
+		});
 	};
 
 	return (
