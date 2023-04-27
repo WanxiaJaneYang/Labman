49720d5ac16d756004ef975f61e2544dbc9f8dbc
WanxiaJaneYang <1016887513@qq.com>
2023-04-28 00:37:11 +0930

    fixed the log_type bug in request

--- LabMan-Backend/src/controllers/request/asyncFuncRequest.js
+++ LabMan-Backend/src/controllers/request/asyncFuncRequest.js
@@ -5,60 +5,57 @@ export async function insertRequestRecord(connection, requestRecord) {
 	return new Promise((resolve, reject) => {
 	  // Insert the record into the requests table
 	  connection.query("INSERT INTO requests SET ?", requestRecord, async (error, result) => {
-		if (error) {
+			if (error) {
 		  console.error(error);
 		  reject(new Error("Failed to insert record into table requests"));
-		} else {
+			} else {
 		  // Return the insert ID
 		  resolve(result.insertId);
-		}
+			}
 	  });
 	});
-  }
-
+}
 
 export async function insertBorrowingRecords(connection, borrowingRequest, amount, current_time) {
 	// Insert borrowingRecord into borrowings table N times with amount=1 per record
 
-		const query = "INSERT INTO borrowings SET ?";
-		const promiseQuery = util.promisify(connection.query).bind(connection);
-		const borrowRecord = {
-			student_id: borrowingRequest.student_id,
+	const query = "INSERT INTO borrowings SET ?";
+	const promiseQuery = util.promisify(connection.query).bind(connection);
+	const borrowRecord = {
+		student_id: borrowingRequest.student_id,
+		type_id: borrowingRequest.type_id,
+		type_name: borrowingRequest.type_name,
+		borrow_amount: amount,
+		returned_amount: 0,
+		borrow_date: current_time,
+		return_date: borrowingRequest.return_date,
+		borrow_status: 0,//  0 = borrowed/unreturned
+		request_id: borrowingRequest.request_id
+	};
+	try {
+		const result = await promiseQuery(query, borrowRecord);
+		const borrow_id = result.insertId;
+
+		// creat a new log for new borrowings
+		const borrowLog = {
 			type_id: borrowingRequest.type_id,
 			type_name: borrowingRequest.type_name,
+			student_id: borrowingRequest.student_id,
 			borrow_amount: amount,
-			returned_amount: 0,
-			borrow_date: current_time,
+			returned_amount:0,
 			return_date: borrowingRequest.return_date,
-			borrow_status: 0,//  0 = borrowed/unreturned
-			request_id: borrowingRequest.request_id
+			log_type: 0,  // 1 = borrow
+			log_time: current_time,
+			borrow_id: borrow_id // Use the request_id from the previous query
 		};
-		try {
-			const result = await promiseQuery(query, borrowRecord);
-			const borrow_id = result.insertId;
 
-			// creat a new log for new borrowings
-			const borrowLog = {
-				type_id: borrowingRequest.type_id,
-				type_name: borrowingRequest.type_name,
-				student_id: borrowingRequest.student_id,
-				borrow_amount: amount,
-				returned_amount:0,
-				return_date: borrowingRequest.return_date,
-				log_type: 1,  // 1 = borrow
-				log_time: current_time,
-				borrow_id: borrow_id // Use the request_id from the previous query
-			};
+		await insertEquipmentLog(connection, borrowLog);
 
-			await insertEquipmentLog(connection, borrowLog);
-
-		} catch (error) {
-			console.error(error);
-			throw new Error("Error inserting borrowing records");
-		}
+	} catch (error) {
+		console.error(error);
+		throw new Error("Error inserting borrowing records");
+	}
 }
-
-
 
 export async function updateRequestStatus(connection, request_id, request_status) {
 	const updateStatusQuery = "UPDATE requests SET request_status = ? WHERE request_id = ?";
@@ -84,6 +81,4 @@ export async function updateRequest(connection, type_id, student_id, type_name, 
 	  console.error(error);
 	  throw new Error("Failed to update request");
 	}
-  }
-  
-
+}
