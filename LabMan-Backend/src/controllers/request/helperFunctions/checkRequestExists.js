b0c32245751df3e2211d1d748eaf0834451d72e9
kaini <a1866621@adelaide.edu.au>
2023-05-06 10:52:34 +0930

    error testing

--- LabMan-Backend/src/controllers/request/helperFunctions/checkRequestExists.js
+++ LabMan-Backend/src/controllers/request/helperFunctions/checkRequestExists.js
@@ -1,11 +1,16 @@
+import errorMessages from "../../../utils/constants/errorMessages.js";
+
 export async function checkRequestExists(connection, request_id) {
 	const getRequestQuery = "SELECT * FROM requests WHERE request_id = ?";
 	try {
 		const [result] = await connection.query(getRequestQuery, [request_id]);
 		if (result.length === 0) {
-			throw new Error("Request does not exist");
+			throw new Error(errorMessages.REQUEST_DOESNOT_EXIST);
 		}
 	} catch (error) {
-		throw new Error("Error checking if request exists");
+		if (Object.values(errorMessages).includes(error.message)) {
+			throw new Error(error.message);
+		}
+		throw new Error("Interval error when checking if request exists: "+error.message);
 	}
 }
--- LabMan-Backend/src/utils/constants/errorMessages.js
+++ LabMan-Backend/src/utils/constants/errorMessages.js
@@ -1,5 +1,6 @@
 const errorMessages = {
-
+REQUEST_DOESNOT_EXIST: "The request does not exist",
+STUDENT_DOESNOT_EXIST: "The student does not exist",
 };
 
 export default errorMessages;
