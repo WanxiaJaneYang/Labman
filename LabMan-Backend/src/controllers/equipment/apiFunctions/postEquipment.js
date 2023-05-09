4be6c8cfafa967878eec154aeba973ff43924795
kaini <a1866621@adelaide.edu.au>
2023-05-09 20:51:27 +0930

    added_time field for equipment

--- LabMan-Backend/src/controllers/equipment/apiFunctions/postEquipment.js
+++ LabMan-Backend/src/controllers/equipment/apiFunctions/postEquipment.js
@@ -1,14 +1,16 @@
 import pool from "../../../utils/MySQL/db.js";
 import errorMessages from "../../../utils/constants/errorMessages.js";
+import moment from "moment";
 
 async function newEquipmentType(req, res) {
 	const { type_name, total_amount, available_amount } = req.body;
 	const removable = true;
 	const reserved_amount = 0;
+	const added_time = moment().format("YYYY-MM-DD HH:mm:ss");
 	try {
 		await pool.query(
-			"INSERT INTO equipment_type (type_name, total_amount, available_amount, removable,reserved_amount) VALUES (?, ?,?, ?, ?)",
-			[type_name, total_amount, available_amount, removable,reserved_amount]
+			"INSERT INTO equipment_type (type_name, total_amount, available_amount, removable,reserved_amount,added_time) VALUES (?, ?,?, ?, ?,?)",
+			[type_name, total_amount, available_amount, removable,reserved_amount,added_time]
 		);
 
 		return res.status(201).json({ message: "Equipment type created successfully" });
