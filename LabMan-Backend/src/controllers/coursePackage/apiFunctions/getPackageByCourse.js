7ad607fd7d9119aafb2d042ae633847d757fbb23
kaini <a1866621@adelaide.edu.au>
2023-05-12 11:57:29 +0930

    add 404

--- LabMan-Backend/src/controllers/coursePackage/apiFunctions/getPackageByCourse.js
+++ LabMan-Backend/src/controllers/coursePackage/apiFunctions/getPackageByCourse.js
@@ -11,6 +11,11 @@ async function getPackageByCourse(req, res) {
 		const query = "SELECT * FROM course_package WHERE course_id = ?";
 		const params = [course_id];
 		const [results] = await pool.query(query, params);
+
+		//404 not found
+		if (results.length === 0) {
+			res.status(404).json(errorMessages.PACKAGE_DOESNOT_EXIST);
+		}
 		return res.status(200).json(results);
 	} catch (error) {
 		console.error(error);
