1817e148f8258d5d74d917a5850a438fc6a9d27e
kaini <a1866621@adelaide.edu.au>
2023-05-12 11:17:23 +0930

    bug fix

--- LabMan-Backend/src/controllers/return_borrowing/helperFunctions/updateBorrowingStatus.js
+++ LabMan-Backend/src/controllers/return_borrowing/helperFunctions/updateBorrowingStatus.js
@@ -4,9 +4,6 @@ async function updateBorrowingStatus(connection, borrow_id, status) {
 		await connection.query(query, [status, borrow_id]);
 
 	} catch (error) {
-		if (Object.values(errorMessages).includes(error.message)) {
-			throw new Error(error.message);
-		}
 		throw new Error("Failed update borrowing status: " + error.message);
 	}
 }
