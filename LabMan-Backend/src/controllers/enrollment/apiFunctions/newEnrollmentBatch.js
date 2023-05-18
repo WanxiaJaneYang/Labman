d6fb20d50e9696d321d69c5c9959cab9f8952f7a
kaini <a1866621@adelaide.edu.au>
2023-05-18 10:38:48 +0930

    add course name to get enroll by student response

--- LabMan-Backend/src/controllers/enrollment/apiFunctions/newEnrollmentBatch.js
+++ LabMan-Backend/src/controllers/enrollment/apiFunctions/newEnrollmentBatch.js
@@ -1,21 +1,24 @@
 import pool from "../../../utils/MySQL/db.js";
 import errorMessages from "../../../utils/constants/errorMessages.js";
-import runTransaction from "../../../utils/MySQL/transaction.js";
+import { checkEnrollmentDuplicate } from "../helperFunctions/checkEnrollmentDuplicate.js";
+import { checkCourseExists } from "../../course/helperFunctions/checkCourseExists.js";
+import { checkUserExists } from "../../users/helperFunctions/checkUserExists.js";
 
 async function newEnrollmentBatch(req, res) {
 	const { course_id } = req.params;
-	const { student_ids } = req.body;
+	const { student_id } = req.body;
 
 	try {
-		await runTransaction(async (connection) => {
-			const enrollmentPromises = student_ids.map((student_id) => {
-				const query = "INSERT INTO enrollment (course_id, student_id) VALUES (?, ?)";
-				const params = [course_id, student_id];
-				return pool.query(query, params);
-			});
+		for (const id of student_id) {
+			await checkUserExists(pool, id);
+			await checkCourseExists(pool, course_id);
+			await checkEnrollmentDuplicate(pool, course_id, id);
 
-			await Promise.all(enrollmentPromises);
-		});
+			const query = "INSERT INTO enrollment (course_id, student_id) VALUES (?, ?)";
+			const params = [course_id, id];
+			await pool.query(query, params);
+		};
+
 		return res.status(201).json({ message: "Bulk enrollments are created successfully" });
 	} catch (error) {
 		console.error(error);
