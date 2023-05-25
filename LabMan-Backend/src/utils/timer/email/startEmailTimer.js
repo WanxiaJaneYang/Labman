0778b7a2d6f0d9bbd918578cf9266532e063c6fa
kaini <a1866621@adelaide.edu.au>
2023-05-25 11:27:04 +0930

    timer: every 9am

--- LabMan-Backend/src/utils/timer/email/startEmailTimer.js
+++ LabMan-Backend/src/utils/timer/email/startEmailTimer.js
@@ -1,11 +1,20 @@
 import { sendEmailsToUsers } from './helpers/sendEmailsToUsers.js';
-
+//UAT environment
+import cron from 'node-cron';
 export function startEmailTimer() {
-    // Set the interval for sending emails (e.g., every 24 hours)
-    const interval = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-
-    // Start the timer
-    setInterval(() => {
+    // Schedule the task to run every day at 9AM
+    cron.schedule('0 9 * * *', () => {
         sendEmailsToUsers();
-    }, interval);
+    });
 }
+
+//testing environment
+// export function startEmailTimer() {
+//     // Set the interval for sending emails (e.g., every 24 hours)
+//     const interval = 30 * 1000; // per 30 seconds
+
+//     // Start the timer
+//     setInterval(() => {
+//         sendEmailsToUsers();
+//     }, interval);
+// }
