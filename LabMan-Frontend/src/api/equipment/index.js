2fd73a26d593f2a6e013f357e62aa03826ffd3cc
WanxiaJaneYang <1016887513@qq.com>
2023-05-26 17:50:34 +0930

    fixed the problem the doesn't get error message correctly from backend to user

--- LabMan-Frontend/src/api/equipment/index.js
+++ LabMan-Frontend/src/api/equipment/index.js
@@ -4,49 +4,78 @@ const API_URL=process.env.REACT_APP_BASE_API_URL;
 const API_URL_EQUIPMENT=API_URL+"/equipment";
 
 export const getEquipmentData=async ()=>{
-	const response = await axios.get(API_URL_EQUIPMENT);
-	if(response.status === 200){
-		return response.data;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.get(API_URL_EQUIPMENT);
+		if(response.status === 200){
+			return response.data;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
 
 export const postEquipment=async (values)=>{
-	const response = await axios.post(API_URL_EQUIPMENT,values);
-	if(response.status === 201){
-		return;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.post(API_URL_EQUIPMENT,values);
+		if(response.status === 201){
+			return;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
 
 export const deleteEquipment=async (id)=>{
-	const response = await axios.delete(API_URL_EQUIPMENT+"/"+id);
-	if(response.status === 200){
-		return;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.delete(API_URL_EQUIPMENT+"/"+id);
+		if(response.status === 200){
+			return;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
 
 export const getEquipmentByTypename=async (type_name)=>{
 	const urlParmas=new URLSearchParams();
 	urlParmas.append("type_name",type_name);
-	const response = await axios.get(API_URL_EQUIPMENT+"?"+urlParmas.toString());
-	if(response.status === 200){
-		return response.data;
-	}else{
-		throw new Error(response.error);
-	}
-	
+	try{
+		const response = await axios.get(API_URL_EQUIPMENT+"?"+urlParmas.toString());
+		if(response.status === 200){
+			return response.data;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
+	}	
 };
 
 export const editEquipment=async (id,values)=>{
-	const response = await axios.put(API_URL_EQUIPMENT+"/"+id,values);
-	if(response.status === 200){
-		return;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.put(API_URL_EQUIPMENT+"/"+id,values);
+		if(response.status === 200){
+			return;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
--- LabMan-Frontend/src/api/log/index.js
+++ LabMan-Frontend/src/api/log/index.js
@@ -5,39 +5,62 @@ const API_URL_LOG_EQUIPMENT=API_URL+"/logs/equipment";
 const API_URL_LOG_REQUEST=API_URL+"/logs/request";
 
 export const getEquipmentLog=async ()=>{
-	const response=await axios.get(API_URL_LOG_EQUIPMENT);
-	if(response.status === 200){
-		return response.data;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.get(API_URL_LOG_EQUIPMENT);
+		if(response.status === 200){
+			return response.data;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
 
 export const getRequestLog=async ()=>{
-	const response=await axios.get(API_URL_LOG_REQUEST);
-	if(response.status === 200){
-		return response.data;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.get(API_URL_LOG_REQUEST);
+		if(response.status === 200){
+			return response.data;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
 
 export const getEquipmentLogByTypenameAndStudentId=async (values)=>{
 	const urlParams= new URLSearchParams(values);
-	const response=await axios.get(API_URL_LOG_EQUIPMENT+"?"+urlParams);
-	if(response.status === 200){
-		return response.data;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response = await axios.get(API_URL_LOG_EQUIPMENT+"?"+urlParams);
+		if(response.status === 200){
+			return response.data;
+		}
+	}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
 
 export const getRequestLogByTypenameAndStudentId=async (values)=>{
 	const urlParams= new URLSearchParams(values);
-	const response=await axios.get(API_URL_LOG_REQUEST+"?"+urlParams);
-	if(response.status === 200){
-		return response.data;
-	}else{
-		throw new Error(response.error);
+	try{
+		const response=await axios.get(API_URL_LOG_REQUEST+"?"+urlParams);
+		if(response.status === 200){
+			return response.data;
+		}}catch(error){
+		if(error.response){
+			throw new Error(error.response.data.error);
+		}else{
+			throw new Error(error.message);
+		}
 	}
 };
