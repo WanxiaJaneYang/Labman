4d45683a467dac46b11b0240fd5b41d2d0347c47
WanxiaJaneYang <1016887513@qq.com>
2023-06-04 17:08:58 +0930

    fixed the bug that admin end new request doesn't check available amount

--- LabMan-Frontend/src/pages/admin/RequestPage/Components/Forms/NewRequestRecordForm/index.js
+++ LabMan-Frontend/src/pages/admin/RequestPage/Components/Forms/NewRequestRecordForm/index.js
@@ -1,8 +1,9 @@
-import { Form, Input, InputNumber, message, Select, Row, Space } from "antd";
+import { Form, Input, InputNumber, message, Select} from "antd";
 import { useEffect, useState } from "react";
 import { MinusCircleOutlined } from "@ant-design/icons";
 import { getCourseListByStudentId } from "../../../../../../api/enrollment";
 import { getPackages, getPackageById } from "../../../../../../api/package";
+import { getEquipmentByTypename } from "../../../../../../api/equipment";
 import { getStudentById } from "../../../../../../api/student";
 
 function NewRequestRecordForm({ form }) {
@@ -50,6 +51,15 @@ function NewRequestRecordForm({ form }) {
 			}else{
 				message.error(error.response.error);
 			}
+		}
+	};
+
+	const getAvailableAmount = async (typename) => {
+		try{
+			const response = await getEquipmentByTypename(typename);
+			return response[0].available_amount;
+		}catch(error){
+			message.error(error.message);
 		}
 	};
 
@@ -131,56 +141,76 @@ function NewRequestRecordForm({ form }) {
 				{(fields, { remove }) => (
 					<>
 						{fields.map(({ key, name, ...restField }) => (
-							<Row key={key} >
-								<Space>
-									<Form.Item
-										{...restField}
-										label="Equipment Type"
-										name={[name, "type_name"]}
-										hidden={true}
-										key={"type_name"+key}
-									/>
-									<Form.Item
-										{...restField}
-										label="Equipment ID"
-										name={[name, "type_id"]}
-										hidden={true}
-										key={"type_id"+key}
-									/>
-									<Form.Item
-										{...restField}
-										label={form.getFieldValue(["request_items", key, "type_name"])+" Amount"}
-										name={[name, "borrow_amount"]}
-										key={"request_amount"+key}
-										rules={[
-											{ required: true },
-											{ validator: (_, value) => {
-												const upper_bound_amount=form.getFieldValue(["request_items", key, "upper_bound_amount"]);
-												if(value>upper_bound_amount){
-													return Promise.reject(
-														new Error("Amount exceeds upper bound("+upper_bound_amount+")!")
-													);
-												}else if(value<=0){
-													return Promise.reject(
-														new Error("Amount must be positive!")
-													);
-												}else{
-													return Promise.resolve();
-												}
-											}},
-										]}
+							<div key={key} >
+								
+								<Form.Item
+									{...restField}
+									label="Equipment Type"
+									name={[name, "type_name"]}
+									hidden={true}
+									key={"type_name"+key}
+								/>
+								<Form.Item
+									{...restField}
+									label="Equipment ID"
+									name={[name, "type_id"]}
+									hidden={true}
+									key={"type_id"+key}
+								/>
+								<Form.Item 
+									{...restField}
+									name={[name, "upper_bound_amount"]}
+									hidden={true}
+									key={"upper_bound_amount"+key}
+								/>
+								<Form.Item
+									{...restField}
+									label={form.getFieldValue(["request_items", key, "type_name"])+" Amount"}
+									name={[name, "borrow_amount"]}
+									key={"request_amount"+key}
+									rules={[
+										{ required: true },
+										{ validator: async(_, value) => {
+											const upper_bound_amount=form.getFieldValue(["request_items", key, "upper_bound_amount"]);
+											const typename=form.getFieldValue(["request_items", key, "type_name"]);
+											const available_amount= await getAvailableAmount(typename);
+											if(value>upper_bound_amount){
+												return Promise.reject(
+													new Error("Amount exceeds upper bound("+upper_bound_amount+")!")
+												);
+											}else if(value>available_amount){
+												return Promise.reject(
+													new Error("Amount exceeds available amount("+available_amount+")!")
+												);
+											}else if(value<=0){
+												return Promise.reject(
+													new Error("Amount must be positive!")
+												);
+											}else{
+												return Promise.resolve();
+											}
+										}},
+									]}
+								>
+									<div 
+										style={{
+											display: "flex",
+											alignItems: "center",
+										}}
 									>
 										<InputNumber />
-									</Form.Item>
-									<Form.Item 
-										{...restField}
-										name={[name, "upper_bound_amount"]}
-										hidden={true}
-										key={"upper_bound_amount"+key}
-									/>
-									<MinusCircleOutlined onClick={() => remove(name)} />
-								</Space>
-							</Row>
+										<MinusCircleOutlined 
+											style={{
+												marginLeft: "10px",
+											}}
+		
+											onClick={() => remove(name)} />
+									</div>
+								</Form.Item>
+								
+								
+								
+							</div>
 						))}
 					</>
 				)}
